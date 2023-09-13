6b77ad5b2f8033ed17e45f6127d84c63572d56e6
ShreyRai <52943457+ShreyRai@users.noreply.github.com>
2023-09-13 17:12:48 +0530

    Add files via upload

---
+++ simple-interest.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#This script calculates Simple Interest
+#Given principal, anuual rate of interest in % and time period in years
+
+#Do not use this in PROD environment
+
+#Author: Shreyash Raichand (CG)
+
+#Input:
+#p :- principal amount
+#r :- rate of interest
+#t :- time period in years
+
+#Output:
+#s :- Simple Interest
+
+read -p "Enter the principal:-  " p
+read -p "Enter the annual rate of interest:-  " r
+read -p "Enter the time period in years:-  " t
+
+let s=$p*$t*$r/100
+
+echo "The Simple Interest is:- $s"
